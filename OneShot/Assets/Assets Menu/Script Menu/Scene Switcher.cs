39b82710d2ee2eebdc7bd3ebffab04606e33ad39
PaulH26 <96866721+PaulH26@users.noreply.github.com>
2023-11-25 09:58:53 +0100

    menu done

--- OneShot/Assets/Assets Menu/Script Menu/Scene Switcher.cs
+++ OneShot/Assets/Assets Menu/Script Menu/Scene Switcher.cs
@@ -5,31 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    public void OptionsGame()
+    public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-    public void Back()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-    }
-
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
-    }
-
-    public void PauseGame1()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-    }
-
-    public void PauseGame2()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-    }
-
-    public void PauseGame3()
+    public void Menu()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
